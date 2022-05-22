0a4e829e94b1662b9754382a3b70a6fcc2b99cbb
Ivan Voznyi <ivan.voznyi92@gmail.com>
2022-05-22 21:52:07 +0300

    add deploy build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,6 @@ enum SlideDirection {
   None = 'none'
 }
 
-enum FromLeft {
-  FromLeft,
-  None,
-}
-
-enum FromRight {
-  FromRight,
-  None,
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -25,8 +15,6 @@ export class AppComponent {
   previousSlide = 0;
   currentSlide = 0;
   slideDirection: SlideDirection = SlideDirection.None;
-  FromLeft: FromLeft = FromLeft.None;
-  FromRight: FromRight = FromRight.None;
 
   slideList = [
     { url: './assets/img/city.jpg' },
@@ -39,29 +27,8 @@ export class AppComponent {
 
   slideLeft() {
     this.slideDirection = SlideDirection.Left;
-    this.FromLeft = FromLeft.FromLeft;
 
     const lastSlideIndex = this.slideList.length - 1;
-
-    if (this.FromRight === FromRight.FromRight) {
-
-      if(this.currentSlide === 0) {
-        this.currentSlide = lastSlideIndex;
-        this.previousSlide  = 0;
-        return
-      }
-
-      this.currentSlide -= 1;
-      this.previousSlide = this.currentSlide + 1;
-
-      if (
-        lastSlideIndex === this.currentSlide ||
-        this.currentSlide === 0
-      ) {
-        this.FromRight = FromRight.None;
-      }
-      return;
-    }
 
     if (this.currentSlide === 0) {
       this.currentSlide = lastSlideIndex;
@@ -81,26 +48,8 @@ export class AppComponent {
 
   slideRight() {
     this.slideDirection = SlideDirection.Right;
-    this.FromRight = FromRight.FromRight;
 
     const lastSlideIndex = this.slideList.length - 1;
-
-    if (this.FromLeft === FromLeft.FromLeft) {
-      if (this.currentSlide === lastSlideIndex) {
-        this.FromLeft = FromLeft.None;
-        this.currentSlide -= 1;
-        this.previousSlide = this.currentSlide + 1;
-        return;
-      }
-
-      this.currentSlide += 1;
-      this.previousSlide = this.currentSlide - 1;
-
-      if (lastSlideIndex === this.currentSlide) {
-        this.FromLeft = FromLeft.None;
-      }
-      return;
-    }
 
     if (this.currentSlide === lastSlideIndex) {
       this.currentSlide = 0;
